448d024620ba52d98cc368dd2427baa0467fe952
Alan <github@personala.co.uk>
2018-06-11 22:05:52 +0100

    first file

---
+++ training.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!--
+training.php - dummy output for github training purposes
+Alan Lee 20180611
+-->
+
+<title>Github training project</title>
+
+<?php
+//VERSION
+//Release1 20180611:    (initial release)
+
+
+//CONSTANTS
+
+
+//INITIALISATION
+
+
+?>
+</head>
+
+
+<body>
+
+<p>Github training project</p>
+<p>one</p>
+<p>two</p>
+<p>three</p>
+
+</body>
+</html>
